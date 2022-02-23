329ce51abf68a27e3285b762ca2a066083e87ede
Ersocaut <leonardo.soy.yo@hotmail.com>
2022-02-23 16:19:15 -0500

    HttpServer

---
+++ src/main/java/edu/escuelaing/arep/server/HttpServer.java
@@ -0,0 +1,100 @@
+package edu.escuelaing.arep.server;
+
+import java.net.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static edu.escuelaing.arep.utils.Constants.TYPE;
+
+public class HttpServer {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+
+    private PrintWriter out;
+    private BufferedReader in;
+
+    private String defaultPage = "HTTP/1.1 200 OK\r\n"
+            + "Content-Type: text/html\r\n"
+            + "\r\n"
+            + "<!DOCTYPE html>"
+            + "<html>"
+            + "<head>"
+            + "<meta charset=\"UTF-8\">"
+            + "<title>Title of the document</title>\n"
+            + "</head>"
+            + "<body>"
+            + "<input>"
+            + "</input>"
+            + "<button>"
+            + "Obtener clima"
+            + "</button>"
+            + "</body>"
+            + "</html>";
+
+    public static OutputStream outputStream;
+
+    public final static Map<String, String> types = new HashMap<String, String>();
+
+    public HttpServer(){
+    }
+
+    public void start() throws IOException, URISyntaxException {
+
+        serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        boolean running = true;
+
+        while (running) {
+            clientSocket = null;
+
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine, outputLine;
+            boolean primeraLinea = true;
+            String file = "";
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Received: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            outputLine = defaultPage;
+
+            out.println(outputLine);
+
+            closeConnection();
+        }
+        serverSocket.close();
+    }
+
+    private void closeConnection(){
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
